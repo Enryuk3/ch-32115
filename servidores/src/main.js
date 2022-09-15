ba82656b8ba09643957999bea21da6f53774f197
Cristopher Guzmán <guzmncristopher@gmail.com>
2022-09-14 23:40:16 -0500

    FIX: port environment

--- servidores/src/main.js
+++ servidores/src/main.js
@@ -6,8 +6,8 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use("/", routes)
 
-const PORT = 5000;
+const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, (req, res) => {
-  console.log('server running on port 5000')
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`)
 });
