5d745d38c353ea962721f2acd443afa6841b3a0e
Cristopher Guzmán <guzmncristopher@gmail.com>
2022-10-13 19:52:48 -0500

    ADD: messages and products model

---
+++ desafio_8/db/models/messagesModel.js
@@ -0,0 +1,20 @@
+import { messages_Config, messages_Server as db } from '../dbConfig.js'
+
+(async function() {
+  try {
+    const isTable = await db.schema.hasTable(messages_Config.table);
+    if (!isTable) {
+      await db.schema.createTable(messages_Config.table, (table) => {
+        table.increments('id').primary().notNullable();
+        table.string('name').notNullable();
+        table.string('message').notNullable();
+        table.string('timestamp').notNullable();
+      })
+      console.log('Tabla de mensajes creada con éxito en SQLite.');
+    } else {
+      console.log('La tabla de mensajes en SQLite ya existe.');
+    }
+  } catch (error) {
+    console.log(error);
+  }
+})()
---
+++ desafio_8/db/models/productsModel.js
@@ -0,0 +1,20 @@
+import { products_Config, products_Server as db } from '../dbConfig.js'
+
+(async function() {
+  try {
+    const isTable = await db.schema.hasTable(products_Config.table);
+    if (!isTable) {
+      await db.schema.createTable(products_Config.table, (table) => {
+        table.increments('id').primary().notNullable();
+        table.string('title').notNullable();
+        table.integer('price').notNullable();
+        table.string('thumbnail').notNullable();
+      })
+      console.log('Tabla de productos creada con éxito en MySQL.');
+    } else {
+      console.log('La tabla de productos en MySQL ya existe.');
+    }
+  } catch (error) {
+    console.log(error);
+  }
+})()
