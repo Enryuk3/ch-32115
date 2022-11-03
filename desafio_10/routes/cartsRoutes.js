3ca4aa8766d32a2f5e1e68b84f706fb144143c61
Cristopher Guzmán <guzmncristopher@gmail.com>
2022-11-02 21:51:16 -0500

    Segunda entrega proyecto

---
+++ desafio_10/routes/cartsRoutes.js
@@ -0,0 +1,26 @@
+import { Router } from "express"
+import CartsMongoDAO from "../persistence/daos/CartsMongoDAO.js"
+
+const router = Router()
+const db = new CartsMongoDAO()
+
+router.post("/", async (req, res) =>
+  db.save(req.body).then((response) => res.json(response))
+)
+router.get("/", async (req, res) =>
+  db.getAll().then((response) => res.json(response))
+)
+router.get("/:id", async (req, res) =>
+  db.getByID(req.params.id).then((response) => res.json(response))
+)
+router.put("/", async (req, res) =>
+  db.update(req.body[0], req.body[1]).then((response) => res.json(response))
+)
+router.delete("/", async (req, res) =>
+  db.deleteAll(req.params.id).then((response) => res.json(response))
+)
+router.delete("/:id", async (req, res) =>
+  db.deleteByID(req.params.id).then((response) => res.json(response))
+)
+
+export default router
---
+++ desafio_10/server.js
@@ -0,0 +1,24 @@
+import express from "express"
+import { connect } from "./persistence/dbConfig.js"
+
+import cartsRoutes from "./routes/cartsRoutes.js"
+import messagesRoutes from "./routes/messagesRoutes.js"
+import productsRoutes from "./routes/productsRoutes.js"
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use("/products", productsRoutes)
+app.use("/carts", cartsRoutes)
+app.use("/messages", messagesRoutes)
+
+const PORT = process.env.PORT || 8080
+
+try {
+  await connect()
+  app.listen(PORT, () => console.log(`Escuchando el puerto ${PORT}`))
+} catch (error) {
+  console.log(error)
+}
