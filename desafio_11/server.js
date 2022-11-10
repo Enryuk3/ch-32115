d87526031ffa265985391b1627418d2d937e80d0
Cristopher Guzmán <guzmncristopher@gmail.com>
2022-11-09 23:50:35 -0500

    ADD: desafio mocks y normalizacion

---
+++ desafio_11/server.js
@@ -0,0 +1,53 @@
+import cors from "cors"
+import http from "http"
+import express from "express"
+import { Server } from "socket.io"
+
+import { connect } from "./persistence/dbConfig.js"
+import MessagesFirebaseDAO from "./persistence/daos/messagesFirebaseDAO.js"
+
+import messagesRoutes from "./routes/messagesRoutes.js"
+import productsRoutes from "./routes/productsRoutes.js"
+import apiRoutes from "./routes/apiRoutes.js"
+
+import { normalizeMessages } from "./utils/normalize.js"
+
+const app = express()
+const messagesDB = new MessagesFirebaseDAO()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use("/products", productsRoutes)
+app.use("/messages", messagesRoutes)
+app.use("/api", apiRoutes)
+
+const httpServer = http.createServer(app)
+const socketServer = new Server(httpServer, { cors: { origin: "*" } })
+
+socketServer.on("connection", (client) => {
+  console.log("Usuario conectado:", client.id)
+
+  messagesDB.getAll().then((messages) => {
+    const normalizedMessages = normalizeMessages(messages)
+    socketServer.sockets.emit("loadMessages", normalizedMessages)
+  })
+
+  client.on("newMessage", (data) => {
+    messagesDB.save(data)
+    messagesDB.getAll().then((messages) => {
+      const normalizedMessages = normalizeMessages(messages)
+      socketServer.sockets.emit("loadMessages", normalizedMessages)
+    })
+  })
+})
+
+const PORT = process.env.PORT || 8080
+
+try {
+  await connect()
+  httpServer.listen(PORT, () => console.log(`Escuchando el puerto ${PORT}`))
+} catch (error) {
+  console.log(error)
+}
---
+++ desafio_11/utils/normalize.js
@@ -0,0 +1,15 @@
+import { normalize, schema } from "normalizr"
+
+export function normalizeMessages(data) {
+  const authorEntity = new schema.Entity("author", { idAtribute: "id" })
+  const textEntity = new schema.Entity("text")
+
+  const mensajesEntity = new schema.Entity("message", {
+    author: authorEntity,
+    text: textEntity,
+  })
+
+  const mensajesNormalized = normalize(data, mensajesEntity)
+
+  return mensajesNormalized
+}
